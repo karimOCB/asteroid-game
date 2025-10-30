3a7a64526b7bc18c071a9504029cc8a225e8896e
Karim <chaya.karim.omar@gmail.com>
2025-10-30 23:09:54 +0000

    B: Display

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
 import pygame
+from constants import * 
 
 def main():
-    print("Starting Asteroids!")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("black")
+        pygame.display.flip()
+
+    print(f"""
+    Starting Asteroids!
+    Screen width: {SCREEN_WIDTH}
+    Screen height: {SCREEN_HEIGHT}
+    """)
+    
 
 
 if __name__ == "__main__":
