9e722fc4b1ddac043adc63acd6509bd2626ec4bd
Karim <chaya.karim.omar@gmail.com>
2025-10-31 13:37:04 +0000

    D: Rotate Player

--- main.py
+++ main.py
@@ -7,17 +7,19 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     clock = pygame.time.Clock()
-    dt = 0
+    dt = 0 
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
         screen.fill("black")
         player.draw(screen)
+        player.update(dt)
         pygame.display.flip()
-        dt= clock.tick(60) / 1000 
-         
+        dt = clock.tick(60) / 1000 
+       
 
     print(f"""
     Starting Asteroids!
